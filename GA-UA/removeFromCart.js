e610ab0a3af729089151a2ea42840e1c20264c8b
DevSantiago <santiago.martinez@adsmurai.com>
2022-06-20 15:16:17 -0500

    Se agrega scripts para GA-UA

---
+++ GA-UA/removeFromCart.js
@@ -0,0 +1,18 @@
+// Measure the removal of a product from a shopping cart.
+dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+dataLayer.push({
+  'event': 'removeFromCart',
+  'ecommerce': {
+    'remove': {                               // 'remove' actionFieldObject measures.
+      'products': [{                          //  removing a product to a shopping cart.
+          'name': 'Triblend Android T-Shirt',
+          'id': '12345',
+          'price': '15.25',
+          'brand': 'Google',
+          'category': 'Apparel',
+          'variant': 'Gray',
+          'quantity': 1
+      }]
+    }
+  }
+});
